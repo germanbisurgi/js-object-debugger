9a320475bef005bb3de394b6698d200391b991d8
gerhus <gebisurgi@gmail.com>
2018-02-14 20:53:34 +0100

    recursive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,98 +3,49 @@ window.onload = function () {
    var loggerContainer = document.querySelector('.logger');
    var logger = new Logger(loggerContainer);
 
-   var myFunction = function () {
-      var self = this;
-   };
+	var myObject = {
+		_propertyA_0 : {
+			_propertyA_1 : {
+				_propertyA_2 : {}
+			}
+		},
+		_propertyB_0 : {
+			_propertyB_1 : {
+				_propertyB_2 : {}
+			}
+		},
+		_propertyC_0 : {
+			_propertyC_1 : {
+				_propertyC2_2 : {
+					_propertyC_3 : {
+						_propertyC_4 : 5
+					}
+				}
+			}
+		}
+	}
 
-   var myObject = {
-      _objectA: {
-         _function: function () {},
-         _boolean: true,
-         _string: 'bla bla',
-         _number: 30,
-         _array: ['banana', 'apple'],
-         _instance: self.myIstance = new myFunction(),
-         _objectB: {
-            _function: function () {},
-            _boolean: true,
-            _string: 'bla bla',
-            _number: 30,
-            _array: ['banana', 'apple'],
-            _instance: self.myIstance = new myFunction(),
-            _objectC: {
-               _function: function () {},
-               _boolean: true,
-               _string: 'bla bla',
-               _number: 30,
-               _array: ['banana', 'apple'],
-               _instance: self.myIstance = new myFunction(),
-               _objectD: {}
+	var depth = 1;
+	var maxDepth = 5;
+
+	var recursive = function (_object, _function) {
+
+		for (var _property in _object) {
+			if (_object.hasOwnProperty(_property)) {
+				_function(_property, _object[_property], depth);
+				if (typeof _object[_property] === 'object' && depth < maxDepth) {
+					depth++;
+                    recursive(_object[_property], _function, depth);
+					depth--;
+                }
             }
-         }
-      },
-      _objectB: {
-         _function: function () {},
-         _boolean: true,
-         _string: 'bla bla',
-         _number: 30,
-         _array: ['banana', 'apple'],
-         _instance: self.myIstance = new myFunction(),
-         _objectB: {
-            _function: function () {},
-            _boolean: true,
-            _string: 'bla bla',
-            _number: 30,
-            _array: ['banana', 'apple'],
-            _instance: self.myIstance = new myFunction(),
-            _objectC: {
-               _function: function () {},
-               _boolean: true,
-               _string: 'bla bla',
-               _number: 30,
-               _array: ['banana', 'apple'],
-               _instance: self.myIstance = new myFunction(),
-               _objectD: {}
-            }
-         }
-      },
-      _objectC: {
-         _function: function () {},
-         _boolean: true,
-         _string: 'bla bla',
-         _number: 30,
-         _array: ['banana', 'apple'],
-         _instance: self.myIstance = new myFunction(),
-         _objectB: {
-            _function: function () {},
-            _boolean: true,
-            _string: 'bla bla',
-            _number: 30,
-            _array: ['banana', 'apple'],
-            _instance: self.myIstance = new myFunction(),
-            _objectC: {
-               _function: function () {},
-               _boolean: true,
-               _string: 'bla bla',
-               _number: 30,
-               _array: ['banana', 'apple'],
-               _instance: self.myIstance = new myFunction(),
-               _objectD: {}
-            }
-         }
-      }
-   };
+		}
+	}
 
-   logger.log(myObject);
+   recursive(myObject, function (_key, _value, _depth) {
+	   // console.log(_key, _value, _depth);
+	   console.log(_key, typeof _value, _depth);
+   });
 
-   var touchHandler = function (_event) {
-      _event.preventDefault();
-      logger.log(_event.touches);
-      console.log(_event.touches)
-   };
-
-   loggerContainer.addEventListener('touchstart', touchHandler);
-   loggerContainer.addEventListener('touchmove', touchHandler);
-   loggerContainer.addEventListener('touchend', touchHandler);
 
 };
