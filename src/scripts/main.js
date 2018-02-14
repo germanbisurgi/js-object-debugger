67e7c402941e0739fd0a67e2ed0a265fb71f464d
gerhus <gebisurgi@gmail.com>
2018-02-14 21:58:01 +0100

    recursive path

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,21 +4,21 @@ window.onload = function () {
    var logger = new Logger(loggerContainer);
 
 	var myObject = {
-		_propertyA_0 : {
-			_propertyA_1 : {
-				_propertyA_2 : {}
+		propertyA_1: {
+			propertyA_2: {
+				propertyA_3: {}
 			}
 		},
-		_propertyB_0 : {
-			_propertyB_1 : {
-				_propertyB_2 : {}
+		propertyB_1: {
+			propertyB_2: {
+				propertyB_3: {}
 			}
 		},
-		_propertyC_0 : {
-			_propertyC_1 : {
-				_propertyC2_2 : {
-					_propertyC_3 : {
-						_propertyC_4 : 5
+		propertyC_1: {
+			propertyC_2: {
+				propertyC_3: {
+					propertyC_4: {
+						propertyC_5: 5
 					}
 				}
 			}
@@ -27,12 +27,20 @@ window.onload = function () {
 
 	var depth = 1;
 	var maxDepth = 5;
+	var path = '';
 
 	var recursive = function (_object, _function) {
-
 		for (var _property in _object) {
 			if (_object.hasOwnProperty(_property)) {
-				_function(_property, _object[_property], depth);
+
+				if (path.split('.').length === depth) {
+					if (path === '') {
+						path += _property;
+					}
+					path += '.' + _property;
+				}
+
+				_function(_property, _object[_property], depth, path);
 				if (typeof _object[_property] === 'object' && depth < maxDepth) {
 					depth++;
                     recursive(_object[_property], _function, depth);
@@ -42,9 +50,9 @@ window.onload = function () {
 		}
 	}
 
-   recursive(myObject, function (_key, _value, _depth) {
-	   // console.log(_key, _value, _depth);
-	   console.log(_key, typeof _value, _depth);
+   recursive(myObject, function (_key, _value, _depth, _path) {
+	   //console.log(_key, typeof _value, _depth, 'root' +  '.' + _key);
+	   console.log(_path);
    });
 
 
