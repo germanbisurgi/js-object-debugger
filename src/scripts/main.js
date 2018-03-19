733a4a2b403519fa56ea75a7ce630aa6b7a20585
germanbisurgi <gebisurgi@gmail.com>
2018-03-19 14:21:42 +0100

    refactored code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,7 +16,7 @@ var CircularClass = function () {
 var circular = new CircularClass();
 
 var object = {
-	live: {
+	mouse: {
 		x: 0,
 		y: 0
 	},
@@ -36,25 +36,19 @@ var object = {
 	function: MyCLass,
 	date: new Date(),
 	regexp: /^\d+$/,
-	promise: new Promise(function (resolve, reject) { /**/ })
+	promise: new Promise(function (resolve, reject) {})
 };
 
-var loggerElement = document.querySelector('.output');
-var loggerRange = document.querySelector('.output-range');
 var objectDebugger = new ObjectDebugger(document.querySelector('.output'));
 
-loggerRange.oninput = function (_event) {
-	loggerElement.scrollTop = Math.floor((loggerElement.scrollHeight - window.innerHeight) * _event.target.value);
-};
-
 document.addEventListener('mousemove', function (event) {
-	objectDebugger.set(object, 'live.x', event.clientX);
-	objectDebugger.set(object, 'live.y', event.clientY);
+	object.mouse.x = event.clientX;
+	object.mouse.y = event.clientY;
 });
 
 function step (timestamp) {
-	objectDebugger.set(object, 'date', new Date());
-	objectDebugger.set(object, 'timestamp', timestamp);
+	object.date = new Date();
+	object.timestamp = timestamp;
 	objectDebugger.print(object, 3);
 	window.requestAnimationFrame(step);
 }
